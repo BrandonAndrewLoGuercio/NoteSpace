4516c9ac1597a1190cee51036c15b0c8914b3346
brandonandrewloguercio <brandonloguercio@yahoo.com>
2015-06-02 15:45:24 -0700

    Add styling with normalize &  scss

--- app/controllers/notes_controller.rb
+++ app/controllers/notes_controller.rb
@@ -1,18 +1,18 @@
 class NotesController < ApplicationController
   before_action :find_note, only: [:show, :edit, :update, :destroy]
   def index
-    @notes = Note.all.order("created_at DESC")
+    @notes = Note.where(user_id: current_user)
   end
 
   def show
   end
 
   def new
-    @note = Note.new
+    @note = current_user.notes.build
   end
 
   def create
-    @note = Note.new(note_params)
+    @note = current_user.notes.build(note_params)
 
     if @note.save
       redirect_to @note
